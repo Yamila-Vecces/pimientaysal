a5d4ed278ef38fa87c335e552d22d1d9afeda051
Yamila-Vecces <yamilavecces@gmail.com>
2021-09-17 19:54:00 -0300

    Subo pimientaysal

---
+++ src/routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const {index, detalle} = require('../controllers/indexController')
+
+/* GET home page. */
+router.get('/', index);
+
+router.get('/detalle/:id', detalle)
+
+module.exports = router;
